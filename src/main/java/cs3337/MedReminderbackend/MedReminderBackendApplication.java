9da7ad76742c462d6973e01f1990a223cb49b50a
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-10-10 13:12:11 -0700

    init spring boot (#1)

---
+++ src/main/java/cs3337/MedReminderbackend/MedReminderBackendApplication.java
@@ -0,0 +1,13 @@
+package cs3337.MedReminderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedReminderBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MedReminderBackendApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/cs3337/MedReminderbackend/MedReminderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cs3337.MedReminderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedReminderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
