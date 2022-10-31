a30b5a8fae566e080d1266f5c31489af87d331d7
kfong00 <kevin.fong00@gmail.com>
2022-10-30 21:24:52 -0700

    Add functions: getDoctors & getPatients #11

--- src/main/java/cs3337/MedReminderbackend/DB/HospitalDB.java
+++ src/main/java/cs3337/MedReminderbackend/DB/HospitalDB.java
@@ -1,6 +1,10 @@
 package cs3337.MedReminderbackend.DB;
 
 import java.util.*;
+
+import cs3337.MedReminderbackend.Model.Doctors;
+import cs3337.MedReminderbackend.Model.Patients;
+
 import java.sql.*;
 
 public class HospitalDB {
@@ -32,6 +36,40 @@ public class HospitalDB {
         }
         return true;
     }
+	
+	public Doctors getDoctors(Integer id) {
+		Doctors d = null;
+		try {
+			String sql = "select * from doctors where id = ?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()) {
+				d = new Doctors(rs.getInt("id"), rs.getString("fname"),rs.getString("lname"),rs.getString("phone"),rs.getString("email"));
+			}
+			pstmt.close();
+		}catch(SQLException e) {
+			return null;
+		}
+		return d;
+	}
+	
+	public Patients getPatients(Integer id) {
+		Patients p = null;
+		try {
+			String sql = "select * from patients where id = ?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()) {
+				p = new Patients(rs.getInt("id"), rs.getString("fname"),rs.getString("lname"),rs.getString("phone"),rs.getString("email"), rs.getInt("primaryDoc"));
+			}
+			pstmt.close();
+		}catch(SQLException e) {
+			return null;
+		}
+		return p;
+	}
 	
 	
 }
