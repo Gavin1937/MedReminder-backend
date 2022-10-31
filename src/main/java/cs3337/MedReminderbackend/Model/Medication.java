80b44bf7f4cd7d7240e917d59be863a1132bd791
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-10-31 00:18:46 -0700

    fix type error in Medication

--- src/main/java/cs3337/MedReminderbackend/Model/Medication.java
+++ src/main/java/cs3337/MedReminderbackend/Model/Medication.java
@@ -1,13 +1,18 @@
 package cs3337.MedReminderbackend.Model;
 
 import java.util.ArrayList;
-
 import org.json.JSONObject;
 
 public class Medication
 {
     
-    public Medication(Integer id, String name, String description, Integer frequency, Integer earlyTime, Integer lateTime){
+    public Medication(
+        Integer id,
+        String name, String description,
+        Integer frequency, Integer earlyTime,
+        Integer lateTime
+    )
+    {
         this.id=id;
         this.name=name;
         this.description=description;
@@ -16,7 +21,8 @@ public class Medication
         this.lateTime=lateTime;
     }
     
-    public JSONObject toJson(){
+    public JSONObject toJson()
+    {
         JSONObject newObj = new JSONObject();
         newObj.put("id", this.getId());
         newObj.put("name", this.getName());
@@ -26,67 +32,91 @@ public class Medication
         newObj.put("late_time", this.getLateTime());
         return newObj;
     }
-
-    public static Medication fromJson(JSONObject json){
-        Medication med = new Medication
-        (json.getInteger("id"), json.getString("name"), json.getString("description"),
-        json.getInteger("frequency"), json.getInteger("early_time"), json.getInteger("late_time"));
+    
+    public static Medication fromJson(JSONObject json)
+    {
+        Medication med = new Medication(
+            json.getInt("id"),
+            json.getString("name"), json.getString("description"),
+            json.getInt("frequency"), json.getInt("early_time"),
+            json.getInt("late_time")
+        );
         return med;
     }
-
-    public ArrayList<Integereger> getTimeInfo(){
-        ArrayList<Integereger> timeInfo = new ArrayList<Integereger>();
+    
+    public ArrayList<Integer> getTimeInfo()
+    {
+        ArrayList<Integer> timeInfo = new ArrayList<Integer>();
         timeInfo.add(this.getFrequency());
         timeInfo.add(this.getEarlyTime());
         timeInfo.add(this.getLateTime());
         return timeInfo;
     }
-
-    public JSONObject getJsonTimeInfo(){
+    
+    public JSONObject getJsonTimeInfo()
+    {
         JSONObject obj = new JSONObject();
         obj.put("frequency", this.frequency);
         obj.put("early_time", this.earlyTime);
         obj.put("late_time", this.lateTime);
         return obj;
     }
-
-    public Integer getId() {
+    
+    public Integer getId()
+    {
         return id;
     }
-    public void setId(Integer id) {
+    public void setId(Integer id)
+    {
         this.id = id;
     }
-    public String getName() {
+    
+    public String getName()
+    {
         return name;
     }
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
-    public String getDescription() {
+    
+    public String getDescription()
+    {
         return description;
     }
-    public void setDescription(String description) {
+    public void setDescription(String description)
+    {
         this.description = description;
     }
-    public Integer getFrequency() {
+    
+    public Integer getFrequency()
+    {
         return frequency;
     }
-    public void setFrequency(Integer frequency) {
+    public void setFrequency(Integer frequency)
+    {
         this.frequency = frequency;
     }
-    public Integer getEarlyTime() {
+    
+    public Integer getEarlyTime()
+    {
         return earlyTime;
     }
-    public void setEarlyTime(Integer earlyTime) {
+    public void setEarlyTime(Integer earlyTime)
+    {
         this.earlyTime = earlyTime;
     }
-    public Integer getLateTime() {
+    
+    public Integer getLateTime()
+    {
         return lateTime;
     }
-    public void setLateTime(Integer lateTime) {
+    public void setLateTime(Integer lateTime)
+    {
         this.lateTime = lateTime;
     }
-
+    
+    
     // private members
     private Integer id = -1;
     private String name = null;
